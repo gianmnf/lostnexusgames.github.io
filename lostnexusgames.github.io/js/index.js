9d1b43bbd751654396ceaff77af0e79afefedd09
gianveloxbr <gianveloxsi@gmail.com>
2017-11-19 17:38:17 -0200

    Site Lançado

---
+++ lostnexusgames.github.io/js/index.js
@@ -0,0 +1,19 @@
+function games(){
+	var x = document.getElementById("games");
+	if(x.style.display === "none"){
+		x.style.display = "block";
+	}
+	else{
+		x.style.display = "none";
+	}
+}
+
+function equipe(){
+	var x = document.getElementById("equipe");
+	if(x.style.display === "none"){
+		x.style.display = "block";
+	}
+	else{
+		x.style.display = "none";
+	}
+}
